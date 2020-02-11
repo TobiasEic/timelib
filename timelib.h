cf83508aaaa917cf1534d3f0d7e2393f47a46ea4
Tobias Eichhorn <t.eichhorn@basecom.de>
2020-02-11 10:20:11 +0100

    outsource functions in timelib.c and add timelib.h

---
+++ timelib.h
@@ -0,0 +1,21 @@
+/*
+ * timelib.h
+ *
+ *  Created on: 11.02.2020
+ *      Author: tobiaseichhorn
+ */
+
+#ifndef TIMELIB_H_
+#define TIMELIB_H_
+
+int get_days_for_month(int month, int year);
+
+int day_of_the_year(int day, int month, int year);
+
+int is_leapyear(int year);
+
+int exists_day(int day, int month, int year);
+
+void input_day(int day, int month, int year);
+
+#endif /* TIMELIB_H_ */
